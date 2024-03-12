f7f827c66d3f4fe806461a877e5ac47d10f11a80
Hoàng Tiến Anh <129145008+HTAnh2003@users.noreply.github.com>
2024-03-12 19:36:07 +0700

    Delete testing directory

--- testing/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import current_datetime,random_trungthuong,random_love #.view vì view.py nằm cùng thư mục với urls.py
-urlpatterns = [
-    path("time/", current_datetime, name="current_datetime"),
-    path("random/", random_trungthuong, name="random_trungthuong"),
-    path("love/", random_love, name="random_love"),
-]
